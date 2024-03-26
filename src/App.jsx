7589f6d61f7db9a8b6522cc66e8c1ee8658ad85c
khojiakbarr <chinozbekov09@gmail.com>
2024-03-26 21:23:17 +0500

    udate

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+const walletContex = createContext();
+function App() {
+  return <h1>HEllo</h1>;
+}
+
+export { walletContex };
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements-react/dist/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "540px" },
+      // => @media (max-width: 540px) { ... }
+    },
+  },
+  darkMode: "class",
+  plugins: [require("tw-elements-react/dist/plugin.cjs")],
+};
